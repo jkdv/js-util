b3fce9bf189b5d7b74ac94b760586972691fdd3d
jkdv <jason.heetae.kim@gmail.com>
2014-06-02 00:06:53 +0900

    Revert d34df65..6aa03d1
    This rolls back to commit d34df65b2a39a40824d686564d29df4308b1aaa6.

--- js/jason.data.js
+++ js/jason.data.js
@@ -1,121 +1,5 @@
 (function(jk, undefined) {
 
-var Node = function(o) {
-	this.data = o;
-	this.prev = null;
-	this.next = null;
-};
-
-/*
- * List
- */
-jk.List = function() {
-	var first = null,
-		last = null,
-		size = 0;
-};
-
-jk.List.prototype.add = function(o) {
-	var node = new Node(o);
-	if (this.last == null) {
-		this.last = node;
-		this.first = node;
-		this.size = 1;
-	} else {
-		this.last.next = node;
-		node.prev = this.last;
-		this.last = node;
-		this.size++;
-	}
-};
-
-jk.List.prototype.get = function(index) {
-	if (index >= this.size || index < 0) {
-		return null;
-	} else {
-		var pointer = this.last;
-		for (var i = this.size - 1; i > index; i--)
-			pointer = pointer.prev;
-		return pointer.data;
-	}
-};
-
-jk.List.prototype.remove = function(o, comparer) {
-	if (arguments.length == 1 &&
-		typeof(arguments[0]) == "number") {
-		if (this.size <= o || o < 0)
-			return null;
-
-		if (this.size == 1) {
-			this.first = null;
-			this.last = null;
-			this.size = 0;
-		} else {
-			var pointer = this.last;
-			for (var i = this.size - 1; i > o; i--)
-				pointer = pointer.prev;
-			if (pointer.prev != null)
-				pointer.prev.next = pointer.next;
-			if (pointer.next != null)
-				pointer.next.prev = pointer.prev;
-			if (this.first == pointer)
-				this.first = this.first.next;
-			if (this.last == pointer)
-				this.last = this.last.prev;
-			pointer.prev = null;
-			pointer.next = null;
-			pointer.null;
-			this.size--;
-		}
-	} else if (arguments.length == 2 &&
-		typeof arguments[1]  == "function") {
-		var pointer = this.last;
-		while (pointer != null) {
-			if (comparer(o) == comparer(pointer.data))
-				break;
-			else
-				pointer = pointer.prev;
-		}
-		if (pointer.prev != null)
-			pointer.prev.next = pointer.next;
-		if (pointer.next != null)
-			pointer.next.prev = pointer.prev;
-		if (this.first == pointer)
-			this.first = this.first.next;
-		if (this.last == pointer)
-			this.last = this.last.prev;
-		pointer.prev = null;
-		pointer.next = null;
-		pointer.null;
-		this.size--;
-	}
-};
-
-jk.List.prototype.contains = function(o, comparer) {
-	if (typeof comparer != "function")
-		return;
-	var pointer = this.last;
-	while (pointer != null) {
-		if (comparer(o) == comparer(pointer.data))
-			return true;
-		else
-			pointer = pointer.prev;
-	}
-	return false;
-};
-
-jk.List.prototype.length = function(o) {
-	return this.size;
-};
-
-jk.List.prototype.printAll = function(printer) {
-	var pointer = this.first;
-	while (pointer != null) {
-		console.log(pointer.data);
-		pointer = pointer.next;
-	}
-};
-
 /*
  * Queue
  */
@@ -125,8 +9,14 @@ jk.Queue = function() {
 		size = 0;
 };
 
+var QueueNode = function(o) {
+	this.data = o;
+	this.prev = null;
+	this.next = null;
+};
+
 jk.Queue.prototype.enqueue = function(o) {
-	var node = new Node(o);
+	var node = new QueueNode(o);
 	if (this.last == null) {
 		this.last = node;
 		this.first = node;
@@ -136,7 +26,7 @@ jk.Queue.prototype.enqueue = function(o) {
 		this.last.prev = node;
 		this.last = node;
 		this.size++;
-	}
+	};
 };
 
 jk.Queue.prototype.dequeue = function() {
@@ -162,23 +52,26 @@ jk.Queue.prototype.length = function() {
 };
 
 jk.Queue.prototype.contains = function(o, comparer) {
+	console.log('This function has been deprecated.');
+
 	if (typeof comparer != "function")
 		return;
+
 	var pointer = this.last;
 	while (pointer != null) {
 		if (comparer(o) == comparer(pointer.data))
 			return true;
 		else
-			pointer = pointer.prev;
-	}
+			pointer = pointer.next;
+	};
 	return false;
 };
 
-jk.Queue.prototype.printAll = function(printer) {
-	if (typeof printer == "function") {
+jk.Queue.prototype.printAll = function(print) {
+	if (typeof print == "function") {
 		var pointer = this.last;
 		while (pointer != null) {
-			console.log(printer(pointer.data));
+			console.log(print(pointer.data));
 			pointer = pointer.next;
 		};
 	};
